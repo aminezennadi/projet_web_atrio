f3fca13fb32e42a12dc4c007593111660b5d42a4
ZENNADI Amine <A.ZENNADI-prestataire@alptis.fr>
2024-12-05 16:45:28 +0100

    API DOC

--- web_atrio/src/main/java/app/controllers/EmploisController.java
+++ web_atrio/src/main/java/app/controllers/EmploisController.java
@@ -3,6 +3,8 @@ package app.controllers;
 import app.models.Emplois;
 import app.models.Personne;
 import app.services.EmploisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +14,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/emplois")
+@Tag(name = "Emploi API", description = "Gérer les emplois")
 public class EmploisController {
 
     @Autowired
     private EmploisService emploisService;
 
     @PostMapping("/assign")
+    @Operation(summary = "Créer un emplois",
+            description = "Ajoute un emplois à une personne existante")
     public ResponseEntity<Emplois> createAndAssignEmploi(
             @RequestParam int personneId,
             @RequestBody Emplois emploi) {
@@ -30,6 +35,8 @@ public class EmploisController {
     }
 
     @GetMapping("/between-dates")
+    @Operation(summary = "Recherche d'un emplois entre 2 dates",
+            description = "Récuperer la liste des emplois d'une personne dans une période donnée")
     public List<Emplois> getEmploisBetweenDates(
             @RequestParam int personneId,
             @RequestParam String startDate,
--- web_atrio/src/main/java/app/controllers/PersonneController.java
+++ web_atrio/src/main/java/app/controllers/PersonneController.java
@@ -2,6 +2,8 @@ package app.controllers;
 
 import app.models.Personne;
 import app.services.PersonneService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,22 +11,29 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/personnes")
+@Tag(name = "Personnes API", description = "Gérer les personnes")
 public class PersonneController {
 
     @Autowired
     private PersonneService personneService;
 
     @PostMapping
+    @Operation(summary = "Créer une nouvelle personne",
+            description = "Ajoute une nouvelle personne avec les détails fournis")
     public Personne addPersonne(@RequestBody Personne personne) {
         return personneService.savePersonne(personne);
     }
 
     @GetMapping("/with-jobs")
+    @Operation(summary = "Liste des personnes avec emplois",
+            description = "Retourne la liste des personnes ayant un emplois en cours")
     public List<Personne> getPersonnesWithJobs() {
         return personneService.getPersonnesWithCurrentJobs();
     }
 
     @GetMapping("/by-entreprise")
+    @Operation(summary = "Liste des personnes par entreprise",
+            description = "Retourne la liste des personnes travaillant dans une entreprise précise")
     public List<Personne> getPersonnesByEntreprise(@RequestParam String entreprise) {
         return personneService.getPersonnesByEntreprise(entreprise);
     }
